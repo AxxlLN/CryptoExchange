fb656fedb9db3f5a52dc26b7724b3657a9052254
AxxlLN <alexnoirmine@gmail.com>
2026-09-17 20:11:26 +0300

    Update 3

--- src/test/java/com/crypto/exchange/ExchangeApplicationTests.java
+++ src/test/java/com/crypto/exchange/ExchangeApplicationTests.java
@@ -2,8 +2,11 @@ package com.crypto.exchange;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExchangeApplicationTests {
 
 	@Test
